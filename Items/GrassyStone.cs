f217bd8ad7b47e23f41155b1a72e41eb6504bfc2
Yamin <41270883+yam12345678@users.noreply.github.com>
2019-03-03 11:11:49 +0000

    Update GrassyStone.cs

--- Items/GrassyStone.cs
+++ Items/GrassyStone.cs
@@ -5,27 +5,39 @@ namespace YaxilMod.Items
 {
     public class GrassyStone : ModItem
     {
-        public override void SetStaticDefaults() {
+        public override void SetStaticDefaults()
+        {
             Tooltip.SetDefault("A special stone, only the bravest can find them...");
         }
 
-        public override void SetDefaults() {
+        public override void SetDefaults()
+        {
             item.width = 20;
             item.height = 20;
             item.maxStack = 99;
             item.value = 100;
             item.rare = 1;
         }
+        
 
-        public override void AddRecipes() {
+        public override void AddRecipes()
+        {
             ModRecipe recipe = new ModRecipe(mod);
-            recipe.AddIngredient(ItemID.DirtBlock);
-            recipe.SetResult(this, 999);
+            recipe.AddIngredient(ItemID.DirtBlock, 1);
+            recipe.SetResult(this, 1);
             recipe.AddRecipe();
             recipe = new ModRecipe(mod);
-            recipe.AddRecipeGroup("YaxilMod:YaxilModItem");
-            recipe.SetResult(this, 999);
+            recipe.AddIngredient(mod.ItemType("GrassyStone"));
+            recipe.AddTile(ItemID.WorkBench);
+            recipe.SetResult(this, 2);
             recipe.AddRecipe();
-                   }
+            recipe = new ModRecipe(mod);
+            recipe.AddIngredient(mod.ItemType("GrassyStone"));
+            recipe.AddTile(mod.TileType("YaxilWorkBench"));
+            recipe.SetResult(this, 3);
+            recipe.AddRecipe();
+        }
+          
     }
-}
+    }
+
